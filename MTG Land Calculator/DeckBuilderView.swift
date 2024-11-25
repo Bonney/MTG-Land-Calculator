d21033ae9434029d3d6b64cd3f666173927195d4
Matt Bonney <bonneym@gmail.com>
2024-11-25 08:51:29 -0500

    App Store 1.0 submission.

--- MTG Land Calculator/DeckBuilderView.swift
+++ MTG Land Calculator/DeckBuilderView.swift
@@ -133,53 +133,16 @@ struct CustomStepper: View {
 }
 
 
+
 struct DeckBuilderView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @State private var viewModel = DeckViewModel()
 
     var body: some View {
         List {
-            Section {
-                Picker("Deck Size", selection: $viewModel.selectedDeckSize) {
-                    ForEach(DeckSize.allCases, id: \.self) { size in
-                        Text("\(size.rawValue) (\(size.cardCount.formatted()))").tag(size)
-                    }
-                }
-                HStack {
-                    VStack {
-                        Text(viewModel.totalSpells.formatted()).foregroundStyle(.primary)
-                        Text("Spells").foregroundStyle(.secondary)
-                    }
-
-                    Slider(value: $viewModel.landRatio, in: 0.1...0.6, step: 0.01)
-
-                    VStack {
-                        Text(viewModel.totalLands.formatted()).foregroundStyle(.primary)
-                        Text("Lands").foregroundStyle(.secondary)
-                    }
-                }
-            }
-
-            Section("Mana Symbols") {
-                ForEach(ManaColor.allCases, id: \.self) { color in
-                    CustomStepper(color: color, viewModel: viewModel)
-                }
-            }
-
-            Section("Suggested Land") {
-                ForEach(ManaColor.allCases, id: \.self) { color in
-                    let count: Int = (viewModel.calculatedLands[color] ?? 0)
-
-                    Label {
-                        LabeledContent {
-                            Text("×\(count.formatted())")
-                        } label: {
-                            Text(color.landName)
-                        }
-                    } icon: {
-                        color.symbolImage
-                    }
-                }
-            }
+            deckSection
+            manaSymbolSection
+            suggestedLandsSection
         }
         .listStyle(.plain)
         .headerProminence(.increased)
@@ -188,6 +151,55 @@ struct DeckBuilderView: View {
         .animation(.smooth, value: viewModel.calculatedLands)
         .navigationTitle("Land Calculator")
         .toolbarTitleDisplayMode(.inline)
+    }
+
+    var deckSection: some View {
+        Section {
+            Picker("Deck Size", selection: $viewModel.selectedDeckSize) {
+                ForEach(DeckSize.allCases, id: \.self) { size in
+                    Text("\(size.rawValue) (\(size.cardCount.formatted()))").tag(size)
+                }
+            }
+            HStack {
+                VStack {
+                    Text(viewModel.totalSpells.formatted()).foregroundStyle(.primary)
+                    Text("Spells").foregroundStyle(.secondary)
+                }
+
+                Slider(value: $viewModel.landRatio, in: 0.1...0.6, step: 0.01)
+
+                VStack {
+                    Text(viewModel.totalLands.formatted()).foregroundStyle(.primary)
+                    Text("Lands").foregroundStyle(.secondary)
+                }
+            }
+        }
+    }
+
+    var manaSymbolSection: some View {
+        Section("Mana Symbols") {
+            ForEach(ManaColor.allCases, id: \.self) { color in
+                CustomStepper(color: color, viewModel: viewModel)
+            }
+        }
+    }
+
+    var suggestedLandsSection: some View {
+        Section("Suggested Land") {
+            ForEach(ManaColor.allCases, id: \.self) { color in
+                let count: Int = (viewModel.calculatedLands[color] ?? 0)
+
+                Label {
+                    LabeledContent {
+                        Text("×\(count.formatted())")
+                    } label: {
+                        Text(color.landName)
+                    }
+                } icon: {
+                    color.symbolImage
+                }
+            }
+        }
     }
 }
 
